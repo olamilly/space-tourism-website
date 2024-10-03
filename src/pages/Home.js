444515741c3c0d91d909d16673272a67a0712824
Mustapha Olamide <olamyy.m@gmail.com>
2024-10-03 22:42:38 +0100

    done with hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,21 @@ function Home(props) {
 			<main>
 				<div className="hero">
 					<div className="text">
-						<p className="w-100">So you want to travel to</p>
-						<h1 className="w-100">SPACE</h1>
-						<p>
-							Let’s face it; if you want to go to space, you might as well
-							genuinely go to outer space and not hover kind of on the edge of
-							it. Well sit back, and relax because we’ll give you a truly out of
-							this world experience!
-						</p>
+						<div className="text-container">
+							<p className="w-100">So you want to travel to</p>
+							<h1 className="w-100">SPACE</h1>
+							<p>
+								Let’s face it; if you want to go to space, you might as well
+								genuinely go to outer space and not hover kind of on the edge of
+								it. Well sit back, and relax because we’ll give you a truly out
+								of this world experience!
+							</p>
+						</div>
 					</div>
-					<div className="button-container">
-						<div className="button">Explore</div>
+					<div className="button d-flex">
+						<div className="button-container">
+							<p className="mb-0">Explore</p>
+						</div>
 					</div>
 				</div>
 			</main>
